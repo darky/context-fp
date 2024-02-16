d8f9ffaf25f8024a34fca4b3b87a3e6470c3ad26
Vladislav Botvin <darkvlados@gmail.com>
2024-02-16 19:05:14 +0300

    basic implementation

---
+++ index.ts
@@ -0,0 +1,107 @@
+const cache = new WeakMap<object, Map<() => unknown, unknown>>()
+
+export function cfp<C, T1, R>(dep1: (ctx: C) => T1, fn: (x1: T1) => R): ((ctx: C) => R) & { raw: (x1: T1) => R }
+export function cfp<C, T1, T2, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  fn: (x1: T1, x2: T2) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2) => R }
+export function cfp<C, T1, T2, T3, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  fn: (x1: T1, x2: T2, x3: T3) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3) => R }
+export function cfp<C, T1, T2, T3, T4, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4) => R }
+export function cfp<C, T1, T2, T3, T4, T5, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  dep5: (ctx: C) => T5,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5) => R }
+export function cfp<C, T1, T2, T3, T4, T5, T6, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  dep5: (ctx: C) => T5,
+  dep6: (ctx: C) => T6,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6) => R }
+export function cfp<C, T1, T2, T3, T4, T5, T6, T7, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  dep5: (ctx: C) => T5,
+  dep6: (ctx: C) => T6,
+  dep7: (ctx: C) => T7,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7) => R }
+export function cfp<C, T1, T2, T3, T4, T5, T6, T7, T8, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  dep5: (ctx: C) => T5,
+  dep6: (ctx: C) => T6,
+  dep7: (ctx: C) => T7,
+  dep8: (ctx: C) => T8,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8) => R }
+export function cfp<C, T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  dep5: (ctx: C) => T5,
+  dep6: (ctx: C) => T6,
+  dep7: (ctx: C) => T7,
+  dep8: (ctx: C) => T8,
+  dep9: (ctx: C) => T9,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8, x9: T9) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8, x9: T9) => R }
+export function cfp<C, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>(
+  dep1: (ctx: C) => T1,
+  dep2: (ctx: C) => T2,
+  dep3: (ctx: C) => T3,
+  dep4: (ctx: C) => T4,
+  dep5: (ctx: C) => T5,
+  dep6: (ctx: C) => T6,
+  dep7: (ctx: C) => T7,
+  dep8: (ctx: C) => T8,
+  dep9: (ctx: C) => T9,
+  dep10: (ctx: C) => T10,
+  fn: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8, x9: T9, x10: T10) => R
+): ((ctx: C) => R) & { raw: (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8, x9: T9, x10: T10) => R }
+export function cfp(...fns: ((...args: unknown[]) => unknown)[]) {
+  if (!fns.length) {
+    throw new Error('cfp initialized without functions')
+  }
+  const raw = fns.at(-1)!
+  const deps = fns.slice(0, -1)
+  return Object.assign(
+    (context: object) => {
+      const contextCache = cache.get(context) ?? cache.set(context, new Map()).get(context)!
+      return raw(
+        ...deps.map(f => {
+          if (contextCache.has(f)) {
+            return contextCache.get(f)
+          }
+          const resp = f(context)
+          contextCache.set(f, resp)
+          return resp
+        })
+      )
+    },
+    { raw }
+  )
+}
---
+++ test.ts
@@ -0,0 +1,194 @@
+import assert from 'node:assert'
+import test from 'node:test'
+import { cfp } from './index.js'
+
+test('cfp initialized without args', async () => {
+  await assert.rejects(async () => (cfp as any)(), new Error('cfp initialized without functions'))
+})
+
+test('basic', async () => {
+  const positiveNumbers = ({ numbers }: { numbers: number[] }) => numbers.filter(n => n > 0)
+  const stringPostfix = () => 'postfix'
+  const positiveNumbersAsString = cfp(positiveNumbers, stringPostfix, (ns, postfix) => `${ns.toString()} ${postfix}`)
+  assert.strictEqual(positiveNumbersAsString({ numbers: [-1, -5, 7, 0, 4] }), '7,4 postfix')
+})
+
+test('raw', async () => {
+  const positiveNumbers = ({ numbers }: { numbers: number[] }) => numbers.filter(n => n > 0)
+  const stringPostfix = () => 'postfix'
+  const positiveNumbersAsString = cfp(positiveNumbers, stringPostfix, (ns, postfix) => `${ns.toString()} ${postfix}`)
+  assert.strictEqual(positiveNumbersAsString.raw([7, 4], 'postfix'), '7,4 postfix')
+})
+
+test('cache', async () => {
+  let called = 0
+  const positiveNumbers = ({ numbers }: { numbers: number[] }) => (called++, numbers.filter(n => n > 0))
+  const positiveNumbersLength = cfp(positiveNumbers, ns => ns.length)
+  const positiveNumbersAsString = cfp(
+    positiveNumbers,
+    positiveNumbersLength,
+    (ns, l) => `${ns.toString()}; length - ${l}`
+  )
+  assert.strictEqual(positiveNumbersAsString({ numbers: [-1, -5, 7, 0, 4] }), '7,4; length - 2')
+  assert.strictEqual(called, 1)
+})
+
+test('di', async () => {
+  const fetchUserFromDB = async (): Promise<{ name: string }> => {
+    throw new Error('should not call')
+  }
+
+  const fetchUser = ({ mockFetchUser }: { mockFetchUser?: typeof fetchUserFromDB }) =>
+    mockFetchUser?.() ?? fetchUserFromDB()
+
+  const helloWorldUser = cfp(fetchUser, user => user.then(({ name }) => `Hello world, ${name}!`))
+
+  assert.strictEqual(await helloWorldUser({ mockFetchUser: async () => ({ name: 'Vasya' }) }), 'Hello world, Vasya!')
+})
+
+test('generics 1', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn = cfp(fn1, ({ a }) => a + 1)
+  assert.strictEqual(fn({ ctx: 1 }), 2)
+})
+
+test('generics 2', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn = cfp(fn1, fn2, ({ a }, { b }) => a + b + 1)
+  assert.strictEqual(fn({ ctx: 1 }), 3)
+})
+
+test('generics 3', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn = cfp(fn1, fn2, fn3, ({ a }, { b }, { c }) => a + b + c + 1)
+  assert.strictEqual(fn({ ctx: 1 }), 4)
+})
+
+test('generics 4', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn = cfp(fn1, fn2, fn3, fn4, ({ a }, { b }, { c }, { d }) => a + b + c + d + 1)
+  assert.strictEqual(fn({ ctx: 1 }), 5)
+})
+
+test('generics 5', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn5 = ({ ctx }: { ctx: number }) => ({ e: ctx })
+  const fn = cfp(fn1, fn2, fn3, fn4, fn5, ({ a }, { b }, { c }, { d }, { e }) => a + b + c + d + e + 1)
+  assert.strictEqual(fn({ ctx: 1 }), 6)
+})
+
+test('generics 6', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn5 = ({ ctx }: { ctx: number }) => ({ e: ctx })
+  const fn6 = ({ ctx }: { ctx: number }) => ({ f: ctx })
+  const fn = cfp(fn1, fn2, fn3, fn4, fn5, fn6, ({ a }, { b }, { c }, { d }, { e }, { f }) => a + b + c + d + e + f + 1)
+  assert.strictEqual(fn({ ctx: 1 }), 7)
+})
+
+test('generics 7', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn5 = ({ ctx }: { ctx: number }) => ({ e: ctx })
+  const fn6 = ({ ctx }: { ctx: number }) => ({ f: ctx })
+  const fn7 = ({ ctx }: { ctx: number }) => ({ g: ctx })
+  const fn = cfp(
+    fn1,
+    fn2,
+    fn3,
+    fn4,
+    fn5,
+    fn6,
+    fn7,
+    ({ a }, { b }, { c }, { d }, { e }, { f }, { g }) => a + b + c + d + e + f + g + 1
+  )
+  assert.strictEqual(fn({ ctx: 1 }), 8)
+})
+
+test('generics 8', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn5 = ({ ctx }: { ctx: number }) => ({ e: ctx })
+  const fn6 = ({ ctx }: { ctx: number }) => ({ f: ctx })
+  const fn7 = ({ ctx }: { ctx: number }) => ({ g: ctx })
+  const fn8 = ({ ctx }: { ctx: number }) => ({ h: ctx })
+  const fn = cfp(
+    fn1,
+    fn2,
+    fn3,
+    fn4,
+    fn5,
+    fn6,
+    fn7,
+    fn8,
+    ({ a }, { b }, { c }, { d }, { e }, { f }, { g }, { h }) => a + b + c + d + e + f + g + h + 1
+  )
+  assert.strictEqual(fn({ ctx: 1 }), 9)
+})
+
+test('generics 9', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn5 = ({ ctx }: { ctx: number }) => ({ e: ctx })
+  const fn6 = ({ ctx }: { ctx: number }) => ({ f: ctx })
+  const fn7 = ({ ctx }: { ctx: number }) => ({ g: ctx })
+  const fn8 = ({ ctx }: { ctx: number }) => ({ h: ctx })
+  const fn9 = ({ ctx }: { ctx: number }) => ({ i: ctx })
+  const fn = cfp(
+    fn1,
+    fn2,
+    fn3,
+    fn4,
+    fn5,
+    fn6,
+    fn7,
+    fn8,
+    fn9,
+    ({ a }, { b }, { c }, { d }, { e }, { f }, { g }, { h }, { i }) => a + b + c + d + e + f + g + h + i + 1
+  )
+  assert.strictEqual(fn({ ctx: 1 }), 10)
+})
+
+test('generics 10', () => {
+  const fn1 = ({ ctx }: { ctx: number }) => ({ a: ctx })
+  const fn2 = ({ ctx }: { ctx: number }) => ({ b: ctx })
+  const fn3 = ({ ctx }: { ctx: number }) => ({ c: ctx })
+  const fn4 = ({ ctx }: { ctx: number }) => ({ d: ctx })
+  const fn5 = ({ ctx }: { ctx: number }) => ({ e: ctx })
+  const fn6 = ({ ctx }: { ctx: number }) => ({ f: ctx })
+  const fn7 = ({ ctx }: { ctx: number }) => ({ g: ctx })
+  const fn8 = ({ ctx }: { ctx: number }) => ({ h: ctx })
+  const fn9 = ({ ctx }: { ctx: number }) => ({ i: ctx })
+  const fn10 = ({ ctx }: { ctx: number }) => ({ j: ctx })
+  const fn = cfp(
+    fn1,
+    fn2,
+    fn3,
+    fn4,
+    fn5,
+    fn6,
+    fn7,
+    fn8,
+    fn9,
+    fn10,
+    ({ a }, { b }, { c }, { d }, { e }, { f }, { g }, { h }, { i }, { j }) => a + b + c + d + e + f + g + h + i + j + 1
+  )
+  assert.strictEqual(fn({ ctx: 1 }), 11)
+})
